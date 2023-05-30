d1237d2005a40fa25d1cf04cc90f84dce71db80e
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-05-29 21:52:38 -0300

    feat: just added export for IUser interface

--- user_api/src/database/entities/user.entity.ts
+++ user_api/src/database/entities/user.entity.ts
@@ -1,6 +1,6 @@
 import mongoose, { Schema, Document } from "mongoose";
 
-interface IUser extends Document {
+export interface IUser extends Document {
   name: string;
   email: string;
   password: string;
