4ed33d3f32464c4d2982ac7e5c5fbb378c04a698
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-05-29 21:52:52 -0300

    feat: added new repository for users

---
+++ user_api/src/database/repositories/users.repository.ts
@@ -0,0 +1,25 @@
+import User, { IUser } from "../entities/user.entity";
+
+class UsersRepository {
+  async create(newUser: IUser) {
+    return await User.create(newUser);
+  }
+
+  async find() {
+    return await User.find();
+  }
+
+  async findById(id: string) {
+    return await User.findById(id);
+  }
+
+  async updateById(id: string, payload: any) {
+    return await User.findByIdAndUpdate(id, payload);
+  }
+
+  async delete(id: string) {
+    return await User.findByIdAndDelete(id);
+  }
+}
+
+export const usersRepository = new UsersRepository();
