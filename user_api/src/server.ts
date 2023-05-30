bcc2afaba8ab3316c91cc682bb9df5132f24b944
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-05-29 21:27:50 -0300

    feat: added connection to database on server

--- user_api/src/server.ts
+++ user_api/src/server.ts
@@ -1,10 +1,13 @@
 import { config } from "dotenv";
 
 import app from "./app";
+import { connectToDatabase } from "./database/config";
 
 config();
 
 (async () => {
+  await connectToDatabase();
+
   app.listen(process.env.APP_PORT || 3333, () => {
     console.log(`App is running on port ${process.env.APP_PORT || 3333}`);
   });
