f4a7ecfb8495bc49844b01f1bbc186b57a1f398a
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-05-29 21:43:49 -0300

    feat: added new entity for user

---
+++ user_api/src/database/entities/user.entity.ts
@@ -0,0 +1,20 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const userSchema: Schema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model<IUser>("User", userSchema);
+
+export default User;
