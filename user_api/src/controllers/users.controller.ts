d413da892c1ca2112e229a55e10039447a42142d
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-05-30 22:15:45 -0300

    feat: added new method on users controller to get user by name

--- user_api/src/controllers/users.controller.ts
+++ user_api/src/controllers/users.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { createUserService } from "../services/users/create-user.service";
+import { findUserByNameService } from "../services/users/find-user-by-name.service";
 
 export class UsersController {
   static async create(req: Request, res: Response) {
@@ -10,5 +11,18 @@ export class UsersController {
     const { message, statusCode, data } = result as any;
 
     res.status(statusCode).json({ message, data });
+  }
+
+  static async findByName(req: Request, res: Response) {
+    const { name } = req.params;
+
+    const result = await findUserByNameService(name);
+
+    const { statusCode, message, data } = result;
+
+    res.status(statusCode).json({
+      message,
+      data,
+    });
   }
 }
