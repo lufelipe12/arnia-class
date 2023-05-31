1f4d51dce3f5602f16ff9689bfed0a5227425f65
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-05-30 21:56:20 -0300

    feat: added new return for function in case of errors

--- user_api/src/services/users/create-user.service.ts
+++ user_api/src/services/users/create-user.service.ts
@@ -5,8 +5,24 @@ export async function createUserService(payload: IUser) {
   try {
     const newUser = await usersRepository.create(payload);
 
-    return newUser;
-  } catch (error) {
-    console.log(error);
+    return {
+      message: "User created.",
+      statusCode: 201,
+      data: newUser,
+    };
+  } catch (error: any) {
+    if (error.errors) {
+      return {
+        message: error.message,
+        statusCode: 400,
+        data: null,
+      };
+    }
+
+    return {
+      message: "Internal server error.",
+      statusCode: 500,
+      data: null,
+    };
   }
 }
