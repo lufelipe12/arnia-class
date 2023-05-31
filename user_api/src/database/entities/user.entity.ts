be0062999f94b2031aa9fbf5a836dd194c956c1f
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-05-30 21:20:24 -0300

    feat: added age, isAdmin and new validations to users schema

--- user_api/src/database/entities/user.entity.ts
+++ user_api/src/database/entities/user.entity.ts
@@ -3,14 +3,18 @@ import mongoose, { Schema, Document } from "mongoose";
 export interface IUser extends Document {
   name: string;
   email: string;
+  age: number;
+  isAdmin: boolean;
   password: string;
 }
 
 const userSchema: Schema = new Schema(
   {
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
+    name: { type: String, required: true, minlength: 3, unique: true },
+    email: { type: String, required: true, minlength: 5 },
+    age: { type: Number, required: true, min: 18 },
+    isAdmin: { type: Boolean, default: false },
+    password: { type: String, required: true, minlength: 5 },
   },
   { timestamps: true }
 );
