2fa932f923322d41fcde091e1ba6da6fd372ea15
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-05-31 22:11:30 -0300

    fix: change name in server.ts

--- user_api/src/server.ts
+++ user_api/src/server.ts
@@ -4,24 +4,23 @@ import app from "./app";
 import { connectToDatabase } from "./database/config";
 import { usersRepository } from "./database/repositories/users.repository";
 import Character from "./database/entities/character.entity";
-import User from "./database/entities/user.entity";
 
 config();
 
 (async () => {
   await connectToDatabase();
 
-  const user = (await usersRepository.findByName("Luiz Felipe")) as any;
+  // const user = (await usersRepository.findByName("Luiz Felipe")) as any;
 
-  const newCharacter = await Character.create({
-    nickname: "Druidzin do mal",
-    vocation: "Druid",
-    user: user?._id,
-  });
+  // const newCharacter = await Character.create({
+  //   nickname: "Pandim",
+  //   vocation: "Druid",
+  //   user: user?._id,
+  // });
 
-  user?.characters.push(newCharacter._id);
+  // user?.characters.push(newCharacter._id);
 
-  await user?.save();
+  // await user?.save();
 
   app.listen(process.env.APP_PORT || 3333, () => {
     console.log(`App is running on port ${process.env.APP_PORT || 3333}`);
