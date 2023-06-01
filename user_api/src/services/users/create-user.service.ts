fb8bcaebddcbf518e838111e9e3156b77a1fef2f
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-05-31 21:24:05 -0300

    feat: added bcrypt to hash password

--- user_api/src/services/users/create-user.service.ts
+++ user_api/src/services/users/create-user.service.ts
@@ -1,9 +1,18 @@
+import bcrypt from "bcryptjs";
+
 import { IUser } from "../../database/entities/user.entity";
 import { usersRepository } from "../../database/repositories/users.repository";
 
 export async function createUserService(payload: IUser) {
   try {
-    const newUser = await usersRepository.create(payload);
+    const { password } = payload;
+
+    const passwordHashed = await bcrypt.hash(password, 10);
+
+    const newUser = await usersRepository.create({
+      ...payload,
+      password: passwordHashed,
+    } as any);
 
     return {
       message: "User created.",
