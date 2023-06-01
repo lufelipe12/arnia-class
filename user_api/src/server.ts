d3ac04145f909270009f156167947865a5770ebd
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-05-31 21:54:49 -0300

    feat: just added code to show an example to create character

--- user_api/src/server.ts
+++ user_api/src/server.ts
@@ -2,11 +2,26 @@ import { config } from "dotenv";
 
 import app from "./app";
 import { connectToDatabase } from "./database/config";
+import { usersRepository } from "./database/repositories/users.repository";
+import Character from "./database/entities/character.entity";
+import User from "./database/entities/user.entity";
 
 config();
 
 (async () => {
   await connectToDatabase();
+
+  const user = (await usersRepository.findByName("Luiz Felipe")) as any;
+
+  const newCharacter = await Character.create({
+    nickname: "Druidzin do mal",
+    vocation: "Druid",
+    user: user?._id,
+  });
+
+  user?.characters.push(newCharacter._id);
+
+  await user?.save();
 
   app.listen(process.env.APP_PORT || 3333, () => {
     console.log(`App is running on port ${process.env.APP_PORT || 3333}`);
