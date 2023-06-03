161ea31d4f41061fdeb36c23996550fb60e74700
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-06-02 21:51:11 -0300

    feat: added two new routes to characters

--- user_api/src/routes/characters.routes.ts
+++ user_api/src/routes/characters.routes.ts
@@ -6,4 +6,7 @@ const charactersRouter = Router();
 
 charactersRouter.post("/", CharactersController.create);
 
+charactersRouter.get("/", CharactersController.findAll);
+charactersRouter.get("/:userId", CharactersController.findByUserId);
+
 export default charactersRouter;
