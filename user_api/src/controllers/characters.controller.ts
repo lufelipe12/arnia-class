6cedaf6ff72e08b3da861f7422a5b643c2a3d5a7
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-06-02 21:51:56 -0300

    feat: added new methods to controller and to repositoru

--- user_api/src/controllers/characters.controller.ts
+++ user_api/src/controllers/characters.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import { createCharacterService } from "../services/characters/create-character.service";
+import { findAllCharactersService } from "../services/characters/find-all-characters.service";
+import { findCharacterByUserIdService } from "../services/characters/find-characters-by-user-id.service";
 
 export class CharactersController {
   static async create(req: Request, res: Response) {
@@ -8,6 +10,30 @@ export class CharactersController {
     const result = await createCharacterService(payload);
 
     const { message, statusCode, data } = result;
+
+    res.status(statusCode).json({
+      message,
+      data,
+    });
+  }
+
+  static async findAll(req: Request, res: Response) {
+    const result = await findAllCharactersService();
+
+    const { statusCode, message, data } = result;
+
+    res.status(statusCode).json({
+      message,
+      data,
+    });
+  }
+
+  static async findByUserId(req: Request, res: Response) {
+    const { userId } = req.params;
+
+    const result = await findCharacterByUserIdService(userId);
+
+    const { statusCode, message, data } = result;
 
     res.status(statusCode).json({
       message,
--- user_api/src/database/repositories/characters.repository.ts
+++ user_api/src/database/repositories/characters.repository.ts
@@ -4,6 +4,14 @@ class CharactersRepository {
   async create(payload: ICharacter) {
     return await Character.create(payload);
   }
+
+  async find() {
+    return await Character.find();
+  }
+
+  async findByUserId(userId: string) {
+    return await Character.find({ user: userId }).populate("user");
+  }
 }
 
 export const charactersRepository = new CharactersRepository();
