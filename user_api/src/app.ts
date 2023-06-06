b9d3cd7b9ecf8e6a4990e1d6a0f7a4fb6cf478cf
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-06-05 21:38:39 -0300

    feat: added middleare globally on app

--- user_api/src/app.ts
+++ user_api/src/app.ts
@@ -1,10 +1,13 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 
 import routes from "./routes";
+import { infoLogRequestMiddleware } from "./middlewares/info-log-request.middleware";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(infoLogRequestMiddleware);
 
 app.use(routes);
 
