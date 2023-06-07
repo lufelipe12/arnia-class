825143993edfe2195cf5d4eb78db02ff83f618e7
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-06-06 22:12:02 -0300

    feat: added new middleware on characters route

--- user_api/src/routes/characters.routes.ts
+++ user_api/src/routes/characters.routes.ts
@@ -1,14 +1,16 @@
 import { Router } from "express";
 
 import { CharactersController } from "../controllers/characters.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
 
 const charactersRouter = Router();
-
-charactersRouter.post("/", CharactersController.create);
 
 charactersRouter.get("/", CharactersController.findAll);
 charactersRouter.get("/:userId", CharactersController.findByUserId);
 
+charactersRouter.use(authMiddleware);
+
+charactersRouter.post("/", CharactersController.create);
 charactersRouter.put("/:id", CharactersController.update);
 
 export default charactersRouter;
