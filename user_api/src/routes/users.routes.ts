49c7b2d27533f8e17ea92f6b6c16ee46a822e864
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-06-07 22:10:02 -0300

    feat: added new route to get profiles and all characters

--- user_api/src/routes/users.routes.ts
+++ user_api/src/routes/users.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
 import { UsersController } from "../controllers/users.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
 
 const usersRouter = Router();
 
 usersRouter.post("/", UsersController.create);
 
-usersRouter.get("/:name", UsersController.findByName);
+usersRouter.get("/", authMiddleware, UsersController.findAll);
+usersRouter.get("/profile", authMiddleware, UsersController.profile);
+usersRouter.get("/:id", UsersController.findById);
 
 export default usersRouter;
