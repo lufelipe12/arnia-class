8a3c07472f02930b719d20d603dbe582978a082f
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-06-20 22:32:02 -0300

    feat: added first schema personal trainer for ifit

---
+++ ifit/src/database/entities/personal-trainer.entity.ts
@@ -0,0 +1,22 @@
+import mongoose, { Schema } from "mongoose";
+import { IPersonalTrainer } from "../../interfaces/ipersonal-trainer.interface";
+
+const personalTrainerSchema: Schema = new Schema(
+  {
+    name: { type: String, required: true, minlength: 3 },
+    email: { type: String, required: true, minlength: 8, unique: true },
+    phoneNumber: { type: String, required: true, minlength: 8 },
+    cref: { type: String, required: true, minlength: 8 },
+    sport: { type: String, required: true },
+    password: { type: String, required: true, minlength: 5 },
+    isActive: { type: Boolean, default: true },
+  },
+  { timestamps: true }
+);
+
+const PersonalTrainer = mongoose.model<IPersonalTrainer>(
+  "PersonalTrainer",
+  personalTrainerSchema
+);
+
+export default PersonalTrainer;
