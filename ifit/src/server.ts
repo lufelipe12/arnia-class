d794699d9158f2beb972d18ee034b4cbaa63cde0
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-06-30 20:56:32 -0300

    fix: fix to show results

--- ifit/src/server.ts
+++ ifit/src/server.ts
@@ -4,9 +4,7 @@ import { databaseConnect } from "./database/config";
 (async () => {
   await databaseConnect();
 
-  const port = process.env.PORT || 3333;
-
-  app.listen(+port, "0.0.0.0", () => {
-    console.log(`App is listening on port: ${port}`);
+  app.listen(process.env.APP_PORT || 3333, () => {
+    console.log(`App is listening on port: ${process.env.APP_PORT || 3333}`);
   });
 })();
